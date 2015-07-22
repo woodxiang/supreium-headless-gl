3106a4577e49363002e574102f9f02013766e6a6
mikolalysenko <mikolalysenko@gmail.com>
2015-07-22 11:53:55 -0500

    begin hooking up conformance suite

--- test/conformance.js
+++ test/conformance.js
@@ -2,8 +2,17 @@ var tape = require('tape')
 var runConformance = require('gl-conformance')
 var gl = require('../index')
 
-//Inject types into global namespaces, required by some conformance tests
+//Inject WebGL types into global namespaces, required by some conformance tests
 WebGLRenderingContext = require('../webgl')
+WebGLBuffer          = WebGLRenderingContext.WebGLBuffer
+WebGLFramebuffer     = WebGLRenderingContext.WebGLFramebuffer
+WebGLProgram         = WebGLRenderingContext.WebGLProgram
+WebGLRenderbuffer    = WebGLRenderingContext.WebGLRenderbuffer
+WebGLShader          = WebGLRenderingContext.WebGLShader
+WebGLTexture         = WebGLRenderingContext.WebGLTexture
+WebGLUniformLocation = WebGLRenderingContext.WebGLUniformLocation
+WebGLActiveInfo      = WebGLRenderingContext.WebGLActiveInfo
+WebGLShaderPrecisionFormat = WebGLRenderingContext.WebGLShaderPrecisionFormat
 
 module.exports = function(filter) {
   return runConformance({
