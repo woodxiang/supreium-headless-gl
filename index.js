5a153da7a0b8ab03e8cd8c959a5c44f9abbdda5d
mikolalysenko <mikolalysenko@gmail.com>
2015-08-25 11:46:59 -0500

    check bounds for width/height

--- index.js
+++ index.js
@@ -12,64 +12,67 @@ function flag(options, name, dflt) {
 }
 
 function createContext(width, height, options) {
+  width  = width|0
+  height = height|0
+  if(!(width >= 0 && height >= 0)) {
+    throw new Error('Invalid dimensions for context')
+  }
 
-    console.log(width, height)
+  var contextAttributes = new webgl.WebGLContextAttributes(
+    flag(options, 'alpha', true),
+    flag(options, 'depth', true),
+    flag(options, 'stencil', false),
+    flag(options, 'antialias', true),
+    flag(options, 'premultipliedAlpha', true),
+    flag(options, 'preserveDrawingBuffer', false),
+    flag(options, 'preferLowPowerToHighPerformance', false),
+    flag(options, 'failIfMajorPerformanceCaveat', false))
 
-    var contextAttributes = new webgl.WebGLContextAttributes(
-      flag(options, 'alpha', true),
-      flag(options, 'depth', true),
-      flag(options, 'stencil', false),
-      flag(options, 'antialias', true),
-      flag(options, 'premultipliedAlpha', true),
-      flag(options, 'preserveDrawingBuffer', false),
-      flag(options, 'preferLowPowerToHighPerformance', false),
-      flag(options, 'failIfMajorPerformanceCaveat', false))
+  //Can only use premultipliedAlpha if alpha is set
+  contextAttributes.premultipliedAlpha =
+    contextAttributes.premultipliedAlpha && contextAttributes.alpha
 
-    //Can only use premultipliedAlpha if alpha is set
-    contextAttributes.premultipliedAlpha =
-      contextAttributes.premultipliedAlpha && contextAttributes.alpha
+  var gl = new webgl.WebGLRenderingContext(
+    width,
+    height,
+    contextAttributes.alpha,
+    contextAttributes.depth,
+    contextAttributes.stencil,
+    contextAttributes.antialias,
+    contextAttributes.premultipliedAlpha,
+    contextAttributes.preserveDrawingBuffer,
+    contextAttributes.preferLowPowerToHighPerformance,
+    contextAttributes.failIfMajorPerformanceCaveat)
 
-    var gl = new webgl.WebGLRenderingContext(
-      width,
-      height,
-      contextAttributes.alpha,
-      contextAttributes.depth,
-      contextAttributes.stencil,
-      contextAttributes.antialias,
-      contextAttributes.premultipliedAlpha,
-      contextAttributes.preserveDrawingBuffer,
-      contextAttributes.preferLowPowerToHighPerformance,
-      contextAttributes.failIfMajorPerformanceCaveat)
+  gl.drawingBufferWidth = width
+  gl.drawingBufferHeight = height
 
-    gl.drawingBufferWidth = width
-    gl.drawingBufferHeight = height
+  gl._ = CONTEXT_COUNTER++
 
-    gl._ = CONTEXT_COUNTER++
+  gl._contextattributes = contextAttributes
 
-    gl._contextattributes = contextAttributes
+  gl._programs      = {}
+  gl._shaders       = {}
+  gl._buffers       = {}
+  gl._textures      = {}
+  gl._framebuffers  = {}
+  gl._renderbuffers = {}
 
-    gl._programs      = {}
-    gl._shaders       = {}
-    gl._buffers       = {}
-    gl._textures      = {}
-    gl._framebuffers  = {}
-    gl._renderbuffers = {}
+  var numAttribs = gl.getParameter(gl.MAX_VERTEX_ATTRIBS)
+  for(var i=0; i<numAttribs; ++i) {
+    gl.disableVertexAttribArray(i)
+    gl.vertexAttrib4f(i, 0, 0, 0, 1)
+  }
 
-    var numAttribs = gl.getParameter(gl.MAX_VERTEX_ATTRIBS)
-    for(var i=0; i<numAttribs; ++i) {
-      gl.disableVertexAttribArray(i)
-      gl.vertexAttrib4f(i, 0, 0, 0, 1)
-    }
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, null)
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+  gl.disable(gl.DEPTH_TEST)
+  gl.disable(gl.STENCIL_TEST)
+  gl.clearColor(0, 0, 0, 0)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
 
-    gl.disable(gl.DEPTH_TEST)
-    gl.disable(gl.STENCIL_TEST)
-    gl.clearColor(0, 0, 0, 0)
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
-
-    return gl
+  return gl
 }
 
 module.exports = createContext
