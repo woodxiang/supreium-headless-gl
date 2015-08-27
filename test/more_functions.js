ac5adcac387fccdaea44f6c0632d79ff4abbf744
mikolalysenko <mikolalysenko@gmail.com>
2015-08-27 17:00:11 -0500

    add more conformance test groups

---
+++ test/more_functions.js
@@ -0,0 +1,3 @@
+require('./util/conformance')(function(str) {
+  return str.indexOf('more_functions') === 0
+})
---
+++ test/more_glsl.js
@@ -0,0 +1,3 @@
+require('./util/conformance')(function(str) {
+  return str.indexOf('more_glsl') === 0
+})
