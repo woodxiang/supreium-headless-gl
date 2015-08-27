b154015a1254c5ce56c7a3e189ada037bdcf5ffa
mikolalysenko <mikolalysenko@gmail.com>
2015-08-27 09:42:18 -0500

    skip context tests involving canvas/dom interactions

--- test/context.js
+++ test/context.js
@@ -1,3 +1,5 @@
+var BLACKLIST = [ 'context_context-type-test' ]
+
 require('./util/conformance')(function(str) {
-  return str.indexOf('context') === 0
+  return str.indexOf('context') === 0 && BLACKLIST.indexOf(str) < 0
 })
