c7c97b5167126d824288609df63b3e3e58a7910c
mikolalysenko <mikolalysenko@gmail.com>
2015-09-24 10:52:55 -0500

    remove failing test case with dom references

--- test/textures.js
+++ test/textures.js
@@ -2,7 +2,8 @@ var BLACKLIST = [
   'textures_compressed-tex-image',
   'textures_copy-tex-image-2d-formats',
   'textures_copy-tex-image-and-sub-image-2d',
-  'textures_tex-image-webgl'
+  'textures_tex-image-webgl',
+  'textures_tex-image-and-uniform-binding-bugs'
 ]
 
 require('./util/conformance')(function(str) {
