819b636e2501e4613e3027cfa85738ac2799313a
Mike Morris <michael.patrick.morris@gmail.com>
2015-12-07 17:45:39 -0800

    initial TravisCI config

---
+++ scripts/install_node.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [ ! -d ~/.nvm ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+fi
+
+source ~/.nvm/nvm.sh
+
+nvm install ${NODE_VERSION}
+nvm alias default ${NODE_VERSION}
+nvm use ${NODE_VERSION}
+
+node --version
+npm --version
