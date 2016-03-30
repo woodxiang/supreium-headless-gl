415097e7841ac9e09409847de09cebcb13d6acc8
mikolalysenko <mikolalysenko@gmail.com>
2016-03-30 10:10:33 -0700

    fix style

--- example/fbo.js
+++ example/fbo.js
@@ -1,20 +1,20 @@
-var createContext = require('../index');
-var utils = require('./utils.js');
+var createContext = require('../index')
+var utils = require('./utils.js')
 
-function main() {
-    // Create context
-    var width = 64
-    var height = 64
-    var gl = createContext(width, height)
+function main () {
+  // Create context
+  var width = 64
+  var height = 64
+  var gl = createContext(width, height)
 
-    // Clear screen to red
-    gl.clearColor(1.0, 0.0, 0.0, 1.0)
-    gl.colorMask(true, true, true, true)
-    gl.clear(gl.COLOR_BUFFER_BIT)
+  // Clear screen to red
+  gl.clearColor(1.0, 0.0, 0.0, 1.0)
+  gl.colorMask(true, true, true, true)
+  gl.clear(gl.COLOR_BUFFER_BIT)
 
-    utils.dumpBuffer(gl, width, height)
+  utils.dumpBuffer(gl, width, height)
 
-    gl.destroy()
+  gl.destroy()
 }
 
-main();
+main()
