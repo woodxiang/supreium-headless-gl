334d149e2b5543ff563788a1fc06089942976925
mikolalysenko <mikolalysenko@gmail.com>
2016-03-30 16:43:11 -0700

    blacklist broken test

--- test/glsl_samplers.js
+++ test/glsl_samplers.js
@@ -1,3 +1,7 @@
+var BLACKLIST = [
+  'glsl_samplers_glsl-function-texture2dprojlod'
+]
+
 require('./util/conformance')(function (str) {
-  return str.indexOf('glsl_samplers') === 0
+  return str.indexOf('glsl_samplers') === 0 && BLACKLIST.indexOf(str) < 0
 })
