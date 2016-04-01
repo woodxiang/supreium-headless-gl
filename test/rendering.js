49ce97656546e3241c0a50a9612588308968217a
mikolalysenko <mikolalysenko@gmail.com>
2016-04-01 14:21:00 -0700

    upgrade rendering blacklist

--- test/rendering.js
+++ test/rendering.js
@@ -1,3 +1,9 @@
+var BLACKLIST = [
+  // BROKEN Uses RGB texture FBOs
+  'rendering_framebuffer-texture-switch',
+  'rendering_framebuffer-switch'
+]
+
 require('./util/conformance')(function (str) {
-  return str.indexOf('rendering') === 0
+  return str.indexOf('rendering') === 0 && BLACKLIST.indexOf(str) < 0
 })
