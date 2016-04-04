69537a2c3659d9494496f16e2398582e0ab338a5
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 15:11:09 -0700

    kill nvm path if already installed

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -3,6 +3,10 @@
 set -e
 set -o pipefail
 
+if [[ -f /usr/local/opt/nvm ]]; then
+  sudo rm -rf /usr/local/opt/nvm
+fi
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,12 @@
 set -e
 set -o pipefail
 
+pwd
+ls
+
 source ~/.nvm/nvm.sh
 nvm use ${NODE_VERSION}
+
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which npm` test
