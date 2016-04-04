dcb6e38eb746064f3cb6b15554c4650c61dcdca5
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 13:00:16 -0700

    try calling xvfb-run directly and skip xvfb

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ source ~/.nvm/nvm.sh
 nvm use ${NODE_VERSION}
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  sudo xvfb-run -n 99 -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
+  sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
 else
   npm test
 fi
