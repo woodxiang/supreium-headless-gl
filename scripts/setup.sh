11d14185e54f3bc52822e08104890fbd8d4b600a
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 14:24:19 -0700

    try adding mesa gles2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,10 +4,10 @@
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 
   # Ensure mason is on the PATH
-  #export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
+  export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
 
   # Install Mesa
-  #mason install mesa 10.4.3
+  mason install mesa 10.4.3
 
   ##############################################################################
   # X Server setup
@@ -17,13 +17,13 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   export DISPLAY=:99.0
 
   # Make sure we're loading the 10.4.3 libs we installed manually
-  #export LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}"
+  export LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}"
 
   # Start the mock X server
-  #if [ -f /etc/init.d/xvfb ] ; then
-  sudo Xvfb :99 -screen 0 1024x768x32 &
-  sleep 5 # sometimes, xvfb takes some time to start up
-  #fi
+  if [ -f /etc/init.d/xvfb ] ; then
+    sudo Xvfb :99 -screen 0 1024x768x32 &
+    sleep 5 # sometimes, xvfb takes some time to start up
+  fi
 
   glxinfo
 fi
