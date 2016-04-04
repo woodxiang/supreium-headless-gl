f430fac7a3ce42b917d04147f05eb1df8172b823
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 11:44:05 -0700

    fix path for tape

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,8 +8,8 @@ nvm use ${NODE_VERSION}
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   source ./scripts/setup.sh
-  #xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
-  node_modules/bin/tape test/*.js
+  # xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
+  xvfb-run -n 99 ./node_modules/bin/tape test/*.js
 else
   npm test
 fi
