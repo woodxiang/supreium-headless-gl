2a0aba76be0928f96e0217e620cdcffd45cd87b3
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 16:06:15 -0700

    try using mason again

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,18 @@ nvm use ${NODE_VERSION}
 
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which node` test/simple-shader.js
+  export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
+
+  # Install Mesa
+  mason install mesa 10.4.3
+
+  # Make sure we're connecting to xvfb
+  export DISPLAY=:99.0
+
+  # Make sure we're loading the 10.4.3 libs we installed manually
+  # export LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}"
+
+  sudo LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --server-num=99 -s "-ac -screen 0 1280x1024x24" `which node` test/simple-shader.js
 else
   npm test
 fi
