41a1106670a5ad2df71cd596c38bcca92523da26
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 11:53:49 -0700

    try running without mason

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 # This script is sourced; do not set -e or -o pipefail here.
 
-git submodule update --init mason
+# git submodule update --init mason
 
 # Ensure mason is on the PATH
-export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
+# export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
 
 # Install Mesa
-mason install mesa 10.4.3
+# mason install mesa 10.4.3
 
 ################################################################################
 # X Server setup
 ################################################################################
 
-# Start the mock X server
-sh -e /etc/init.d/xvfb start
-sleep 2 # sometimes, xvfb takes some time to start up
-
 # Make sure we're connecting to xvfb
 export DISPLAY=:99.0
 
 # Make sure we're loading the 10.4.3 libs we installed manually
-export LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}"
+# export LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}"
+
+# Start the mock X server
+sh -e /etc/init.d/xvfb start
+sleep 5 # sometimes, xvfb takes some time to start up
 
 glxinfo
--- scripts/test.sh
+++ scripts/test.sh
@@ -7,9 +7,11 @@ source ~/.nvm/nvm.sh
 nvm use ${NODE_VERSION}
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  source ./scripts/setup.sh
-  # xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
-  xvfb-run ./node_modules/bin/tape test/*.js
+  #source ./scripts/setup.sh
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 5
+  xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
 else
   npm test
 fi
