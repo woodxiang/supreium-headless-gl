77e95b9cb8ce489886b99afaa564443faa282d6f
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 13:24:22 -0700

    xvfb not finding path to node for some reason

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -4,7 +4,10 @@ set -e
 set -o pipefail
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-source ~/.nvm/nvm.sh
+
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+  source ~/.nvm/nvm.sh
+fi
 
 nvm install ${NODE_VERSION}
 nvm alias default ${NODE_VERSION}
