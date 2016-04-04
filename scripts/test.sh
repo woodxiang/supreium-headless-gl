09a6fd80a8dd5de8119d01c34c7a09c36dcc8a77
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 11:28:23 -0700

    fiddling with linux set up

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,15 +3,13 @@
 set -e
 set -o pipefail
 
-if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-    source ./scripts/setup.sh
-fi
-
 source ~/.nvm/nvm.sh
 nvm use ${NODE_VERSION}
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
+  source ./scripts/setup.sh
+  #xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
+  node_modules/bin/tape test/*.js
 else
   npm test
 fi
