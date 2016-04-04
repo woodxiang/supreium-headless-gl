44297c6ec1d5ccf932ee7ca99976c2ef76ac69fd
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 16:44:35 -0700

    try modifying npm loading script, upgrade mesa version

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -6,7 +6,11 @@ set -o pipefail
 sudo rm -rf /usr/local/opt/nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
-source ~/.nvm/nvm.sh
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+  source ~/.nvm/nvm.sh
+else
+  source ~/.bashrc
+fi
 
 nvm install ${NODE_VERSION}
 nvm alias default ${NODE_VERSION}
--- scripts/test.sh
+++ scripts/test.sh
@@ -14,7 +14,7 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 
   # Set up mason
   export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
-  mason install mesa 10.4.3
+  mason install mesa 11.1.2
 
   sudo LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which glxinfo`
 
