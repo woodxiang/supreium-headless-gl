6336dc881b3c14221630302cd0a4e81795ef2e53
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 12:16:23 -0700

    run xvfb as root

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,9 +9,9 @@ nvm use ${NODE_VERSION}
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   # source ./scripts/setup.sh
   export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
+  sudo /etc/init.d/xvfb start
   sleep 5
-  xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
+  sudo xvfb-run -s "-ac -screen 0 1280x1024x24" node_modules/bin/tape test/*.js
 else
   npm test
 fi
