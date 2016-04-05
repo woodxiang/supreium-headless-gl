21215bdaefd9e33b5ad26938e471f8978c8c9fed
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 18:24:27 -0700

    adding back in source for nvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,12 @@
 
 set -o pipefail
 
+if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+  source ~/.bashrc
+else
+  source ~/.nvm/nvm.sh
+fi
+
 nvm install ${NODE_VERSION}
 nvm alias default ${NODE_VERSION}
 nvm use ${NODE_VERSION}
