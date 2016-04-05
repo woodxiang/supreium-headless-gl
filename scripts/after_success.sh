fd8ecfc1f1ef5935333c7c3c5b29f174be9bdbcd
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 21:36:02 -0700

    call prebuild directly from npm script on publishing a new version

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -e
 set -o pipefail
 
 # Inspect binary.
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    source ~/.nvm/nvm.sh
     ldd ./build/Release/webgl.node
 else
+    source ~/.bashrc
     otool -L ./build/Release/webgl.node
 fi
 
@@ -16,11 +17,5 @@ PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 if [[ ${TRAVIS_TAG} == v${PACKAGE_JSON_VERSION} ]] || test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE" && [[ ${COVERAGE} == false ]]; then
     source ~/.nvm/nvm.sh
     nvm use ${NODE_VERSION}
-
-    npm install --compile --strip
-    ./node_modules/.bin/prebuild --upload ${GITHUB_TOKEN}
-
-    rm -rf build
-    npm install --no-compile
-    npm test
+    npm run prebuild
 fi
