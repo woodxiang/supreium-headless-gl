2a415461f3dddd49900e086e83a8013263b1e312
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 17:18:32 -0700

    fix up mason version

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,11 +14,11 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 
   # Set up mason
   export PATH="`pwd`/mason:${PATH}" MASON_DIR="`pwd`/mason"
-  mason install mesa 11.1.2
+  mason install mesa 10.5.2
 
-  sudo LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which glxinfo`
+  sudo LD_LIBRARY_PATH="`mason prefix mesa 10.5.2`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which glxinfo`
 
-  sudo LD_LIBRARY_PATH="`mason prefix mesa 10.4.3`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which node` test/simple-shader.js
+  sudo LD_LIBRARY_PATH="`mason prefix mesa 10.5.2`/lib:${LD_LIBRARY_PATH:-}" xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which node` test/simple-shader.js
 else
   npm test
 fi
