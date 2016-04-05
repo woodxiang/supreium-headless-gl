e5337b846945fb601b6bfe7a202b9ccbdf79d5db
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 17:04:12 -0700

    why is nvm not working now

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -4,15 +4,13 @@ set -e
 set -o pipefail
 
 sudo rm -rf /usr/local/opt/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   source ~/.nvm/nvm.sh
 else
   source ~/.bashrc
 fi
-
-which nvm
 
 nvm install ${NODE_VERSION}
 nvm alias default ${NODE_VERSION}
