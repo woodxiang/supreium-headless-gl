7b6a32636cd03c030bfb6c74c0d44c3868ceb069
mikolalysenko <mikolalysenko@gmail.com>
2016-04-04 20:50:47 -0700

    remove mason dependency

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,7 +12,7 @@ nvm use ${NODE_VERSION}
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which glxinfo`
 
-  sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which node` test/simple-shader.js
+  sudo xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which npm` test
 else
   npm test
 fi
