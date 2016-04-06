ade98fe4c2d25bfcffd5979e7513198f5bb390f4
mikolalysenko <mikolalysenko@gmail.com>
2016-04-06 14:14:00 -0700

    travis builds not working for os x for some lame reason

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -13,7 +13,7 @@ nvm use --delete-prefix ${NODE_VERSION}
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
-#if [[ ${COMMIT_MESSAGE} == ${PACKAGE_JSON_VERSION} ]]; then
+if [[ ${COMMIT_MESSAGE} == ${PACKAGE_JSON_VERSION} ]]; then
   echo "running prebuild"
   node ./node_modules/prebuild/bin.js --all --strip -u ${GITHUB_TOKEN}
-#fi
+fi
