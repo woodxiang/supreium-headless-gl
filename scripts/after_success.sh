56b9060c6f7fb1535da94fe04f62ba662c8c1359
mikolalysenko <mikolalysenko@gmail.com>
2016-04-06 12:08:14 -0700

    typo in prebuild script

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -12,7 +12,7 @@ fi
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
-if [[ ${COMMIT_MESSAGE} == v${PACKAGE_JSON_VERSION} ]]; then
+if [[ ${COMMIT_MESSAGE} == ${PACKAGE_JSON_VERSION} ]]; then
   echo "running prebuild"
   nvm use --delete-prefix ${NODE_VERSION}
   node ./node_modules/prebuild/bin.js --all --strip -u ${GITHUB_TOKEN}
