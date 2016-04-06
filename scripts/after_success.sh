9020b57e61e66607b8fdbc9acabca6078feb0bf5
mikolalysenko <mikolalysenko@gmail.com>
2016-04-06 11:53:34 -0700

    fix after_success check

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -12,10 +12,7 @@ fi
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
-echo ${COMMIT_MESSAGE}
-echo ${PACKAGE_JSON_VERSION}
-
-if [[ ${TRAVIS_TAG} == v${PACKAGE_JSON_VERSION} ]]; then
+if [[ ${COMMIT_MESSAGE} == v${PACKAGE_JSON_VERSION} ]]; then
   echo "running prebuild"
   nvm use --delete-prefix ${NODE_VERSION}
   node ./node_modules/prebuild/bin.js --all --strip -u ${GITHUB_TOKEN}
