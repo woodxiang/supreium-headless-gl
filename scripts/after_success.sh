c8f2194aa20fad498a6478a4ec43117f46d77d21
mikolalysenko <mikolalysenko@gmail.com>
2016-04-05 22:24:24 -0700

    v3.0.0

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -14,7 +14,11 @@ fi
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
-if [[ ${TRAVIS_TAG} == v${PACKAGE_JSON_VERSION} ]] || test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE" && [[ ${COVERAGE} == false ]]; then
+echo ${TRAVIS_TAG}
+echo v${PACKAGE_JSON_VERSION}
+
+if [[ ${TRAVIS_TAG} == v${PACKAGE_JSON_VERSION} ]]; then
+    echo "running prebuild"
     nvm use --delete-prefix ${NODE_VERSION}
     npm run prebuild
 fi
