86cc291ed9b2a2382a37cb1cae8d44f64bc61904
mikolalysenko <mikolalysenko@gmail.com>
2016-04-05 21:48:44 -0700

    [publish binary]

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -o pipefail
+set -eo pipefail
 
 # Inspect binary.
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
@@ -15,7 +15,6 @@ COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
 if [[ ${TRAVIS_TAG} == v${PACKAGE_JSON_VERSION} ]] || test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE" && [[ ${COVERAGE} == false ]]; then
-    source ~/.nvm/nvm.sh
     nvm use --delete-prefix ${NODE_VERSION}
     npm run prebuild
 fi
