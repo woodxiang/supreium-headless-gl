ae48949c2d3cd2a8e517bced19f72030f3d5e23b
Vladimir Agafonkin <agafonkin@gmail.com>
2017-01-14 00:34:33 +0200

    try blind-fixing the AppVeyor build (#99)

--- browser_index.js
+++ browser_index.js
@@ -16,10 +16,10 @@ function createContext (width, height, options) {
   canvas.height = height
 
   try {
-    gl = canvas.getContext('experimental-webgl', options)
+    gl = canvas.getContext('webgl', options)
   } catch (e) {
     try {
-      gl = canvas.getContext('webgl', options)
+      gl = canvas.getContext('experimental-webgl', options)
     } catch (e) {
       return null
     }
