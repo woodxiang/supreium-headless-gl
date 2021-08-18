8a16a5e96af434dd694b44ae8d6787dc3da0d9fe
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2021-08-18 13:26:53 -0400

    Add node 16 to CI

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -14,9 +14,9 @@ PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
 if [[ ${COMMIT_MESSAGE} == ${PACKAGE_JSON_VERSION} ]]; then
   echo "running prebuild"
-  node ./node_modules/prebuild/bin.js -t 8.0.0 -t 9.0.0 -t 10.0.0 -t 11.0.0 -t 12.0.0 -t 14.0.0 --strip -u ${GITHUB_TOKEN}
-  
+  node ./node_modules/prebuild/bin.js -t 8.0.0 -t 9.0.0 -t 10.0.0 -t 11.0.0 -t 12.0.0 -t 14.0.0 -t 16.0.0 --strip -u ${GITHUB_TOKEN}
+
   if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-    node ./node_modules/prebuild/bin.js --libc=musl -t 8.0.0 -t 9.0.0 -t 10.0.0 -t 11.0.0 -t 12.0.0 -t 14.0.0 --strip -u ${GITHUB_TOKEN}
+    node ./node_modules/prebuild/bin.js --libc=musl -t 8.0.0 -t 9.0.0 -t 10.0.0 -t 11.0.0 -t 12.0.0 -t 16.0.0 --strip -u ${GITHUB_TOKEN}
   fi
 fi
