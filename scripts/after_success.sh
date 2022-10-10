efd4aa32455490886637e368ce2eef9cd7b0a441
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2022-10-10 15:14:26 -0400

    Each NODE_VERSION is responsible for building its own prebuilt binary
    This is to potentially work around the openssl_fips variable issue with node-gyp

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -11,12 +11,13 @@ fi
 
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
+PREBUILD_TARGET="${NODE_VERSION}.0.0"
 
 if [[ ${COMMIT_MESSAGE} == ${PACKAGE_JSON_VERSION} ]]; then
   echo "running prebuild"
-  node ./node_modules/prebuild/bin.js -t 14.0.0 -t 16.0.0 -t 18.0.0 --strip -u ${GITHUB_TOKEN}
+  node ./node_modules/prebuild/bin.js -t ${PREBUILD_TARGET} --strip -u ${GITHUB_TOKEN}
 
   if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-    node ./node_modules/prebuild/bin.js --libc=musl -t 16.0.0 -t 18.0.0 --strip -u ${GITHUB_TOKEN}
+    node ./node_modules/prebuild/bin.js --libc=musl -t ${PREBUILD_TARGET} --strip -u ${GITHUB_TOKEN}
   fi
 fi
