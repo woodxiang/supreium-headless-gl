f038e776ba11fea62a442e74277edabb102ebc26
Jamie Madill <null77@gmail.com>
2025-01-12 11:20:07 -0500

    Add TypeScript type definitions file based on @types/gl.

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare namespace createContext {
+  interface STACKGL_destroy_context {
+      destroy(): void;
+  }
+
+  interface STACKGL_resize_drawingbuffer {
+      resize(width: GLint, height: GLint): void;
+  }
+
+  interface StackGLExtension {
+      getExtension(extensionName: "STACKGL_destroy_context"): STACKGL_destroy_context | null;
+      getExtension(extensionName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer | null;
+  }
+
+  const WebGLRenderingContext: WebGLRenderingContext & StackGLExtension & {
+      new(): WebGLRenderingContext & StackGLExtension;
+      prototype: WebGLRenderingContext & StackGLExtension;
+  };
+
+  const WebGL2RenderingContext: WebGL2RenderingContext & StackGLExtension & {
+      new(): WebGL2RenderingContext & StackGLExtension;
+      prototype: WebGL2RenderingContext & StackGLExtension;
+  };
+}
+
+declare function createContext(
+  width: number,
+  height: number,
+  options?: WebGLContextAttributes,
+): WebGLRenderingContext & createContext.StackGLExtension;
+
+export = createContext;
